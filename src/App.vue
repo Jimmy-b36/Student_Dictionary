0a8bd889cfe9bea5d06dbb9edf89e47d8e9b74c4
James Ball <jamie.j.ball@gmail.com>
2025-04-04 09:32:15 -0400

    feat: add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
 <template>
-  <RouterView />
+  <div class="app-container">
+    <Navbar v-if="showNavbar" />
+    <RouterView />
+  </div>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+const showNavbar = computed(() => {
+  return (
+    !route.path.startsWith('/login') &&
+    !route.path.startsWith('/register') &&
+    !route.path.startsWith('/admin/login')
+  )
+})
+</script>
+
+<style></style>
